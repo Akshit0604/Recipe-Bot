bf48b56c6ec13e73ec769df2b6d8cb9f7da1c043
Akshit Pandharkar <akshit.pan@gmail.com>
2024-07-25 14:30:31 +0530

    Update env.js to make API keys hidden before making repo public

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const process={
     
-    OPENAI_API_KEY0: 'sk-proj-YbzeaObPs0kYqJrkCld7T3BlbkFJsznW7rqnqV8IYhUFLBxG',
-    OPENAI_API_KEY1: 'sk-proj-YbzeaObPs0kYqJrkCld7T3BlbkFJsznW7rqnqV8IYhUFLBxG',
-    OPENAI_API_KEY2: 'sk-proj-YbzeaObPs0kYqJrkCld7T3BlbkFJsznW7rqnqV8IYhUFLBxG'  
+    OPENAI_API_KEY0: 'YOUR_API_KEY',
+    OPENAI_API_KEY1: 'YOUR_API_KEY',
+    OPENAI_API_KEY2: 'YOUR_API_KEY'  
 }
